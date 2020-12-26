83f750a45ea305c2f296e8c81f6a19111792b40b
Rahul Dahal <rdaahal@gmail.com>
2020-12-26 20:18:38 +0545

    fix: import Link from react-router

--- src/Components/LeagueCard.js
+++ src/Components/LeagueCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "@reach/router";
+import { Link } from "react-router-dom";
 
 const LeagueCard = ({ leagueName }) => {
   const leagueTrimmed = leagueName.toLowerCase().replace(" ", "");
